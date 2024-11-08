86edcdd4ae53e15b48cdbd0b0aac80d737423a15
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-11-07 22:49:41 -0300

    adicionado autorizações

--- school_contebras_project/settings.py
+++ school_contebras_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kb^n626u=nio@d&@7_e#=^z1uzzkh%)ku3_8)z7nc@aqen#wu(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
